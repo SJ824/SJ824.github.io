951973fa5f73a9392fce88884d59b9e1f8830658
Sungjun <SJ824>
2026-08-27 16:31:50 +0900

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
   // add toggle functionality to abstract, award and bibtex buttons
   $("a.abstract").click(function () {
-    $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).closest(".publication-entry").find(".abstract.hidden").toggleClass("open");
+    $(this).closest(".publication-entry").find(".award.hidden.open").toggleClass("open");
+    $(this).closest(".publication-entry").find(".bibtex.hidden.open").toggleClass("open");
   });
   $("a.award").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
+    $(this).closest(".publication-entry").find(".abstract.hidden.open").toggleClass("open");
+    $(this).closest(".publication-entry").find(".award.hidden").toggleClass("open");
+    $(this).closest(".publication-entry").find(".bibtex.hidden.open").toggleClass("open");
   });
   $("a.bibtex").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
+    $(this).closest(".publication-entry").find(".abstract.hidden.open").toggleClass("open");
+    $(this).closest(".publication-entry").find(".award.hidden.open").toggleClass("open");
+    $(this).closest(".publication-entry").find(".bibtex.hidden").toggleClass("open");
   });
   $("a").removeClass("waves-effect waves-light");
 
